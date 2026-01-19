1b4f69b657045f91a2686c01c77e44e6a13783cf
Czar68 <1968czar@gmail.com>
2026-01-19 06:51:46 -0500

    Change model from llama-3.1-sonar-large to small

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ If any field is not visible or determinable, use null for that field. Be precise
                 'Content-Type': 'application/json'
             },
             json={
-                'model': 'llama-3.1-sonar-large-128k-online',
+                'model': ''model': 'llama-3.1-sonar-small-128k-online',',
                 'messages': [
                     {
                         'role': 'user',
