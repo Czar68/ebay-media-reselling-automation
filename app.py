fa7fb24d8b15c7b312dd2b916d221e958dc17cc0
Czar68 <1968czar@gmail.com>
2026-01-19 07:03:33 -0500

    Change model from 'llama-3.1-sonar-small-128k-online' to 'sonar-pro'

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ If any field is not visible or determinable, use null for that field. Be precise
                 'Content-Type': 'application/json'
             },
             json={
-                'model': 'llama-3.1-sonar-small-128k-online',
+                'model': 'sonar-pro',
                 'messages': [
                     {
                         'role': 'user',
